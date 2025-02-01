df8c7c31964b037c705bd881c9c571827d0d60ce
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2025-02-01 22:43:43 +0530

    js01

---
+++ Js/01.js
@@ -0,0 +1,24 @@
+var x = 10;
+if (true) {
+  var x = 20;  // Same 'x' variable gets reassigned
+}
+console.log(x); // 20
+
+
+
+let x = 10;
+if (true) {
+  let x = 20;  // This 'x' is block-scoped
+  console.log(x); // 20
+}
+console.log(x); // 10
+
+
+
+const x = 10;
+// x = 20;  // Error: Assignment to constant variable.
+
+const obj = { name: "Sandeep" };
+obj.name = "Sachin";  // Allowed, as the object itself is mutable
+console.log(obj.name); // "Sachin"
+
