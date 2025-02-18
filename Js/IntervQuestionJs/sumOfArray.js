4a29e5fb210e9a7776651eec175049e1c5c0f585
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2025-02-18 19:58:51 +0530

    Sum Of Array

---
+++ Js/IntervQuestionJs/sumOfArray.js
@@ -0,0 +1,8 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, ];
+
+const sumOfArray = (array) => {
+     return array.reduce((a, b) => a + b);
+} 
+
+console.log(sumOfArray(array));
+
--- Js/TopJs/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-   const map = new Map();
-   map.set('name', 'John');
-   map.set('age', 30);
-   map.set('city', 'New York');
-   
-//    console.log(map.get('name'));
-   console.log(map.set('name'));
