79d98806f605be7a224d964c09414c15d15e0afa
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2025-02-26 16:21:57 +0530

    E & O

---
+++ Js/IntervQuestionJs/e&o.js
@@ -0,0 +1,7 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const even = arr.filter((num) => num % 2 === 0);
+const odd = arr.filter((num) => num % 2 !== 0);
+
+console.log(even);  
+console.log(odd);
